7bd9b4dff037ecaa011cfe992d434d077a7b127b
sonsuhyune <s3267s@naver.com>
2020-08-08 15:38:49 +0900

    suhyun-camera test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+
+
+    ImageButton imageButton8 = null;
+    ImageView iv = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setup();
+        imageButton8.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+        });
     }
+
+    private void setup()
+    {
+        imageButton8 = (ImageButton)findViewById(R.id.imageButton8);
+        iv = (ImageView)findViewById(R.id.iv);
+    }
+
+
+
+
+
+
+
+
+
+
 }
