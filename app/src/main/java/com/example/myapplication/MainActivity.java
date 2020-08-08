9504726aaf238200e0b0864fa682aff7cb397850
sonsuhyune <s3267s@naver.com>
2020-08-08 16:04:39 +0900

    suhyun-add toolbar

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toolbar;
 
 public class MainActivity extends Activity {
 
@@ -17,12 +20,15 @@ public class MainActivity extends Activity {
     ImageButton imageButton8 = null;
     ImageView iv = null;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.myAppName);
+        setActionBar(toolbar);
         setup();
         imageButton8.setOnClickListener(new View.OnClickListener()
         {
