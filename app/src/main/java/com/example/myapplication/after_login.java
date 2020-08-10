e6718c06559fb2c09fe27de418c15a95f809cdc8
sonsuhyune <s3267s@naver.com>
2020-08-10 16:23:40 +0900

    suhyun-화면전

---
+++ app/src/main/java/com/example/myapplication/after_login.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class after_login extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.after_login);
+    }
+}
