2b1c626dbc4b28f45df0b9afdfab7b713e2b08e4
sonsuhyune <s3267s@naver.com>
2020-08-10 15:51:28 +0900

    suhyun-화면 안뜨는 문제 해결

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,47 +11,16 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends Activity {
-
-
-
-    ImageButton cameraButton = null;
-    ImageView iv = null;
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+public class MainActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.myAppName);
-        setContentView(toolbar);
-        setup();
-        cameraButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(intent);
-            }
-        });
     }
-
-    private void setup()
-    {
-        cameraButton = (ImageButton)findViewById(R.id.cameraButton);
-        iv = (ImageView)findViewById(R.id.iv);
-    }
-
-
-
-
-
-
 
 
 
