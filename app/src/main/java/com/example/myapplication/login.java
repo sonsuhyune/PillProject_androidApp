f0844b43f93fa9a24de043932093551439900f9a
sonsuhyune <s3267s@naver.com>
2020-08-11 13:44:20 +0900

    suhyun-로그인, 회원가입 화면 추

--- app/src/main/java/com/example/myapplication/login.java
+++ app/src/main/java/com/example/myapplication/login.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +15,14 @@ public class login  extends AppCompatActivity {
         setContentView(R.layout.login);
     }
 
-
+    public void temp_login_button(View v) {
+        Intent intent = new Intent(getApplicationContext(), after_login.class);
+        startActivity(intent);
+        Toast.makeText(getApplicationContext(),"로그인하기가 눌렸습니다.", Toast.LENGTH_SHORT).show();
+    }
+    public void signup_button(View v) {
+        Intent intent = new Intent(getApplicationContext(), signup.class);
+        startActivity(intent);
+        Toast.makeText(getApplicationContext(),"회원가입 하기가 눌렸습니다.", Toast.LENGTH_SHORT).show();
+    }
 }
