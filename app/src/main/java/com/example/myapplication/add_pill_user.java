8fe665fc65eba727185766f3a043fd17cb40dd11
sonsuhyune <s3267s@naver.com>
2020-08-22 15:30:56 +0900

    갤러리에서 사진 받기

--- app/src/main/java/com/example/myapplication/add_pill_user.java
+++ app/src/main/java/com/example/myapplication/add_pill_user.java
@@ -1,19 +1,149 @@
 package com.example.myapplication;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
 
 public class add_pill_user extends AppCompatActivity {
 
+    private Button button;
+    static final String TAG = "ProfileActivityTAG";
+    //RequestCode
+    private String mTmpDownloadImageUri;
+    private Bitmap img;
+    private ImageView mImageView;
+    final static int PICK_IMAGE = 1; //갤러리에서 사진선택
+    final static int CAPTURE_IMAGE = 2;  //카메라로찍은 사진선택
+    private String mCurrentPhotoPath;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mImageView = findViewById(R.id.imageView);
         setContentView(R.layout.add_pill_user);
+        button = findViewById(R.id.button); ////
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override //이미지 불러오기기
+            public void onClick(View v) {
+                photoDialogRadio(); //갤러리에서 불러오기 or 사진찍어서 불러오기
+            }
+        });
     }
+    @Override //갤러리에서 이미지 불러온 후 행동
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE && resultCode == Activity.RESULT_OK) {
+            try {
+                InputStream in = getContentResolver().openInputStream(data.getData());
+                img = BitmapFactory.decodeStream(in);
+                in.close();
+                // 이미지 표시
+                mImageView.setImageBitmap(img);
+                Log.d(TAG, "갤러리 inputStream: " + data.getData());
+                Log.d(TAG, "갤러리 사진decodeStream: " + img);
 
+                mTmpDownloadImageUri = null;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == CAPTURE_IMAGE && resultCode == Activity.RESULT_OK) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    File file = new File(mCurrentPhotoPath);
+                    InputStream in = getContentResolver().openInputStream(Uri.fromFile(file));
+                    img = BitmapFactory.decodeStream(in);
+                    mImageView.setImageBitmap(img);
+                    in.close();
+
+                    mTmpDownloadImageUri = null;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    private void photoDialogRadio() {
+        final CharSequence[] PhotoModels = {"갤러리에서 가져오기", "카메라로 촬영 후 가져오기", "기본사진으로 하기"};
+        AlertDialog.Builder alt_bld = new AlertDialog.Builder(this);
+        //alt_bld.setIcon(R.drawable.icon);
+        alt_bld.setTitle("프로필사진 설정");
+        alt_bld.setSingleChoiceItems(PhotoModels, -1, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int item) {
+                Toast.makeText(add_pill_user.this, PhotoModels[item] + "가 선택되었습니다.", Toast.LENGTH_SHORT).show();
+                if (item == 0) { //갤러리
+                    Intent intent = new Intent();
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+                    startActivityForResult(intent, PICK_IMAGE);
+                } else if (item == 1) { //카메라찍은 사진가져오기
+                    takePictureFromCameraIntent();
+                } else { //기본화면으로하기
+                    mImageView.setImageResource(R.drawable.camera2);
+                    img = null;
+                    mTmpDownloadImageUri = null;
+                }
+            }
+        });
+        AlertDialog alert = alt_bld.create();
+        alert.show();
+    }
+    private void takePictureFromCameraIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.myapplication.provider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAPTURE_IMAGE);
+            }
+        }
+    }
+    //카메라로 촬영한 이미지를파일로 저장해주는 함수
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
     public void after_back(View v) {
         Intent intent = new Intent(getApplicationContext(), add_pill.class);
         startActivity(intent);
@@ -22,4 +152,6 @@ public class add_pill_user extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         Toast.makeText(getApplicationContext()," 뒤로가기가 눌렸습니다.", Toast.LENGTH_SHORT).show();
     }
+
+
 }
