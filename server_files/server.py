b182bb2f437e4d2dd773deab73736bcc680ecf83
SeonjeongHwang <tjswjd0228@gmail.com>
2020-09-12 14:01:29 +0900

    modify server.py

--- server_files/server.py
+++ server_files/server.py
@@ -21,28 +21,29 @@ def get_bytes_stream(sock, length):
     return buf[:length]
 
 host = '203.255.176.79'
-port = 8088 
- 
+port = 8088
+
 server_sock = socket.socket(socket.AF_INET)
 server_sock.bind((host, port))
-server_sock.listen(1)
+server_sock.listen(100)
 
-print("기다리는 중")
-client_sock, addr = server_sock.accept()
-print('Connected by', addr, '\n')
+idx = 0
+while True:
+    idx += 1
+    print("기다리는 중")
+    client_sock, addr = server_sock.accept()
 
-len_bytes_string = bytearray(client_sock.recv(1024))[2:]
-len_bytes = len_bytes_string.decode("utf-8")
-length = int(len_bytes)
+    len_bytes_string = bytearray(client_sock.recv(1024))[2:]
+    len_bytes = len_bytes_string.decode("utf-8")
+    length = int(len_bytes)
 
-img_bytes = get_bytes_stream(client_sock, length)
-print(img_bytes)
-print(len(img_bytes))
+    img_bytes = get_bytes_stream(client_sock, length)
 
-with open("img1.png", "wb") as writer:
-    writer.write(img_bytes)
-    
-print("Done")
+    with open("pill_img_from_server/img"+str(idx)+".png", "wb") as writer:
+        writer.write(img_bytes)
 
-client_sock.close()
+    print("pill_img_from_server/img"+str(idx)+".png"+" is saved")
+
+    client_sock.close()
+
 server_sock.close()
